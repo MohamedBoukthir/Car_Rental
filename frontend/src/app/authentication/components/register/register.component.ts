5568378c4a129744623188a70bdecaedc46973b7
MohamedBoukthir <medboukthir7@gmail.com>
2024-02-17 16:13:42 +0100

    Refactor register component HTML and TypeScript files

--- frontend/src/app/authentication/components/register/register.component.ts
+++ frontend/src/app/authentication/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+ registerForm!: FormGroup;
+
+
+  constructor(
+    private fb: FormBuilder
+    ) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ["", [Validators.required], Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]*$')],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+
 
 }
