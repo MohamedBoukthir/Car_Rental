87badc71ae014dec2e39bdec7e6840ff8e9a8abf
MohamedBoukthir <medboukthir7@gmail.com>
2024-02-19 17:54:56 +0100

    Create Admin account

--- backend/src/main/java/com/mohamed/repositories/UserRepository.java
+++ backend/src/main/java/com/mohamed/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mohamed.repositories;
 
 import com.mohamed.entities.User;
+import com.mohamed.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    User findByUserRole(UserRole userRole);
 }
--- backend/src/main/java/com/mohamed/services/authentication/AuthenticationServiceImpl.java
+++ backend/src/main/java/com/mohamed/services/authentication/AuthenticationServiceImpl.java
@@ -5,6 +5,7 @@ import com.mohamed.dto.UserDto;
 import com.mohamed.entities.User;
 import com.mohamed.enums.UserRole;
 import com.mohamed.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,20 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService{
 
     private final UserRepository userRepository;
+
+    @PostConstruct
+    public void createAdminAccount(){
+        User adminAccount = userRepository.findByUserRole(UserRole.ADMIN);
+        if (adminAccount == null) {
+            User newAdminAccount = new User();
+            newAdminAccount.setName("Admin");
+            newAdminAccount.setEmail("admin@gmail.com");
+            newAdminAccount.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            newAdminAccount.setUserRole(UserRole.ADMIN);
+            userRepository.save(newAdminAccount);
+            System.out.println("ADMIN Account Created Successfully.");
+        }
+    }
 
     @Override
     public UserDto createCustomer(RegisterRequest registerRequest) {
