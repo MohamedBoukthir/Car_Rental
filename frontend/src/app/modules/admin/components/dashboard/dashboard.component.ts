f5f1257a283fa810cd7bcfcfd549b137f0733357
MohamedBoukthir <medboukthir7@gmail.com>
2024-02-24 14:53:24 +0100

    Add getAllCars() function and display cars in dashboard component

--- frontend/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  cars: any = [];
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.getAllCars();
+  }
+
+  getAllCars() {
+    this.adminService.getAllCars().subscribe((res) => {
+      console.log(res);
+      res.forEach((element: any) => {
+        // Explicitly define the type of 'element' as 'any'
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg;
+        this.cars.push(element);
+      });
+    });
+  }
 }
--- frontend/src/app/modules/admin/services/admin.service.ts
+++ frontend/src/app/modules/admin/services/admin.service.ts
@@ -6,27 +6,30 @@ import { StorageService } from '../../../authentication/services/storage/storage
 const BASE_URL = 'http://localhost:8080/api/v1/admin/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  
+  // function to add a new car
   addCar(carDto: any): Observable<any> {
     return this.http.post(BASE_URL + 'add-car', carDto, {
-      headers: this.createAuthorizationHeader()
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  // function to get all cars
+  getAllCars(): Observable<any> {
+    return this.http.get(BASE_URL + 'cars', {
+      headers: this.createAuthorizationHeader(),
     });
   }
 
   createAuthorizationHeader(): HttpHeaders {
     let authHeaders: HttpHeaders = new HttpHeaders();
     return authHeaders.set(
-      'Authorization' ,
+      'Authorization',
       'Bearer ' + StorageService.getToken()
     );
   }
-
 }
