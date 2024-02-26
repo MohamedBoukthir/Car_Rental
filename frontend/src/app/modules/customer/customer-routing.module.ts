65e526a3e563228f069cae3ba7b00daefe666cf9
MohamedBoukthir <medboukthir7@gmail.com>
2024-02-26 18:36:47 +0100

    Add booking functionality to customer dashboard

--- frontend/src/app/modules/customer/customer-routing.module.ts
+++ frontend/src/app/modules/customer/customer-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../admin/components/dashboard/dashboard.component';
 import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
+import { BookingComponent } from './components/booking/booking.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: CustomerDashboardComponent}
+  {path: 'dashboard', component: CustomerDashboardComponent},
+  {path: 'book/:id', component: BookingComponent}
 ];
 
 @NgModule({
--- frontend/src/app/modules/customer/customer.module.ts
+++ frontend/src/app/modules/customer/customer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
+import { BookingComponent } from './components/booking/booking.component';
 
 
 @NgModule({
   declarations: [
-    CustomerDashboardComponent
+    CustomerDashboardComponent,
+    BookingComponent
   ],
   imports: [
     CommonModule,
