e812701d45d00b8dec0bc403fe86ef2a8d6fe443
MohamedBoukthir <medboukthir7@gmail.com>
2024-03-01 17:59:18 +0100

    Add search feature form

--- frontend/src/app/modules/admin/components/search/search.component.ts
+++ frontend/src/app/modules/admin/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,131 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+
+  searchForm!: FormGroup;
+  listOfOption: Array<{ label: string; value: string }> = [];
+  listOfBrands = [
+    'BMW',
+    'Audi',
+    'Mercedes',
+    'Toyota',
+    'Honda',
+    'Nissan',
+    'Mazda',
+    'Ford',
+    'Chevrolet',
+    'Jeep',
+    'Kia',
+    'Hyundai',
+    'Volkswagen',
+    'Fiat',
+    'Peugeot',
+    'Renault',
+    'Skoda',
+    'Volvo',
+    'Suzuki',
+    'Subaru',
+    'Mitsubishi',
+    'Lexus',
+    'Infiniti',
+    'Acura',
+    'Cadillac',
+    'Buick',
+    'Lincoln',
+    'Chrysler',
+    'Dodge',
+    'Tesla',
+    'Jaguar',
+    'Land Rover',
+    'Porsche',
+    'Ferrari',
+    'Maserati',
+    'Lamborghini',
+    'Bentley',
+    'Rolls Royce',
+    'McLaren',
+    'Bugatti',
+    'Lotus',
+    'Alfa Romeo',
+    'Aston Martin',
+    'Morgan',
+    'Mini',
+    'Smart',
+    'Opel',
+    'Seat',
+    'Citroen',
+    'Dacia',
+    'Lada',
+    'Saab',
+    'Daewoo',
+    'Lancia',
+    'Tata',
+    'Mahindra',
+    'Isuzu',
+    'Proton',
+    'Geely',
+    'Chery',
+    'Great Wall',
+    'Zotye',
+    'Changan',
+    'BYD',
+    'Brilliance',
+    'JAC',
+    'Haval',
+    'Dongfeng',
+    'Foton',
+    'GAC',
+    'BAIC',
+    'Chery',
+    'Other',
+  ];
+  listOfType = ['Hybird', 'Electric', 'Petrol', 'Diesel', 'Gas', 'Other'];
+  listOfColor = [
+    'Red',
+    'Blue',
+    'Green',
+    'Yellow',
+    'Black',
+    'White',
+    'Silver',
+    'Grey',
+    'Brown',
+    'Orange',
+    'Purple',
+    'Pink',
+    'Other',
+  ];
+  listOfTransmission = ['Automatic', 'Manual', 'Semi-Automatic'];
+
+  constructor(
+    private fb: FormBuilder
+  ) { 
+    this.searchForm = this.fb.group({
+      brand: [''],
+      type: [''],
+      transmission: [''],
+      color: [''],
+    })
+  }
+
+  search() {
+    console.log(this.searchForm.value);
+  }
+
+  get transmission() {
+    return this.searchForm.get('transmission');
+  }
+
+  get brand() {
+    return this.searchForm.get('brand');
+  }
+
+  get type() {
+    return this.searchForm.get('type');
+  }
+
+  get color() {
+    return this.searchForm.get('color');
+  }
 
 }
